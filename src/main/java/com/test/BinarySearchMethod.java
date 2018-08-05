4f2c24e991ac5e44b0f439beb6ce8b29808531d6
charbel <charbel@nat-wireless-guest-reg-153-50.bu.edu>
2018-08-05 18:34:43 -0400

    test Java 8

---
+++ src/main/java/com/test/BinarySearchMethod.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.Arrays;
+
+public class BinarySearchMethod {
+
+	
+	private static int[] arry = {1, 2, 3, 4, 5, 6};
+	
+	public static void main(String[] args)
+	{
+		System.out.println(Arrays.binarySearch(arry, 8));
+	}
+	
+}
